0afd37170c85d4990a0f4b132a5cf5a6d653a474
Shaz Sedighzadeh <shaz.digital@gmail.com>
2021-07-22 16:10:35 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn product design and vaping 
         </a>
       </header>
     </div>
